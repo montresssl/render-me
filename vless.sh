da3864b1d7dfb5224960d7cea2fc2af2c5798bb5
montresssl <124798706+montresssl@users.noreply.github.com>
2023-08-01 15:16:01 +0630

    Update vless.sh

--- vless.sh
+++ vless.sh
@@ -29,24 +29,17 @@ echo -e "# ${GREEN}YouTube 频道${PLAIN}: https://www.youtube.com/@misaka-blog 
 echo "#############################################################"
 echo ""
 
-yellow "使用前请注意："
-red "1. 我已知悉本项目有可能触发 Deepnote 封号机制"
-red "2. 我不保证脚本其搭建节点的稳定性"
-#read -rp "是否安装脚本？ [Y/N]：" yesno
-yesno = "Y"
-if [[ yesno =~ "Y"|"y" ]]; then
-    rm -f web config.json
-    yellow "开始安装..."
-    wget -O temp.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
-    unzip temp.zip
-    rm -f temp.zip
-    mv v2ray web
-    read -rp "请设置UUID（如无设置则使用脚本默认的）：" uuid
-    if [[ -z $uuid ]]; then
-        uuid="8d4a8f5e-c2f7-4c1b-b8c0-f8f5a9b6c384"
-    fi
-    rm -f config.json
-    cat << EOF > config.json
+
+rm -f web config.json
+   
+wget -O temp.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+unzip temp.zip
+rm -f temp.zip
+mv v2ray web
+uuid="8d4a8f5e-c2f7-4c1b-b8c0-f8f5a9b6c384"
+   
+rm -f config.json
+cat << EOF > config.json
 {
     "log": {
         "loglevel": "warning"
@@ -94,14 +87,11 @@ if [[ yesno =~ "Y"|"y" ]]; then
     ]
 }
 EOF
-    nohup ./web run &>/dev/null & ls
-    #nohup relay connect --name modsbots & ls
-    green "Deepnote v2ray 已安装完成！"
-    yellow "请认真阅读项目博客说明文档，配置出站链接！"
-    yellow "别忘记给项目点一个免费的Star！"
-    echo ""
-    yellow "更多项目，请关注：小御坂的破站"
-else
-    red "已取消安装，脚本退出！"
-    exit 1
-fi
+nohup ./web run &>/dev/null & ls
+#nohup relay connect --name modsbots & ls
+green "Deepnote v2ray 已安装完成！"
+yellow "请认真阅读项目博客说明文档，配置出站链接！"
+yellow "别忘记给项目点一个免费的Star！"
+echo ""
+yellow "更多项目，请关注：小御坂的破站"
+
